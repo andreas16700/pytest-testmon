b239b7280d5dd2168552496933471b010ab537fa
Bilgehan Akcan <78697606+bilgehanAkcan@users.noreply.github.com>
2025-11-05 01:27:52 +0100

    Remove API proxy configuration from vite.config.js
    Removed server proxy configuration for API.

--- ez-viz/client/vite.config.js
+++ ez-viz/client/vite.config.js
@@ -3,13 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://ezmon.aloiz.ch',
-        changeOrigin: true,
-        secure: true,
-      }
-    }
-  }
 })
